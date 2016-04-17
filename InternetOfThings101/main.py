df6fd0ce51c67ee4f550731a768259ae1f710cb1
jose luis <meraz1990@hotmail.com>
2016-04-17 20:12:54 +0000

    Hello
    Signed-off-by: jose luis <meraz1990@hotmail.com>

---
+++ InternetOfThings101/main.py
@@ -0,0 +1,29 @@
+
+
+
+ 
+#!/usr/bin/python
+
+
+import signal
+import sys
+import time
+
+
+def interruptHandler(signal, Frame):
+
+	sys.exit(0)
+
+if __name__== '__main__':
+
+	signal.signal(signal.SIGINT, interruptHandler)
+
+
+	while True:
+
+		print "Hello Internet of Things 101"
+		time.sleep(5)
+
+
+#End of File
+
