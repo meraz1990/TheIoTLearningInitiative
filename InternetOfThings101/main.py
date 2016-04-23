99b9e59626b5a4dccace40740e2b57317e83f08a
Jose Luis Meraz <meraz1990@hotmail.com>
2016-04-23 16:50:46 +0000

    sd

--- InternetOfThings101/main.py
+++ InternetOfThings101/main.py
@@ -1,29 +1,45 @@
-
-
-
- 
 #!/usr/bin/python
 
-
+import paho.mqtt.client as paho
+import psutil
 import signal
 import sys
 import time
 
+from threading import Thread
 
-def interruptHandler(signal, Frame):
+def interruptHandler(signal, frame):
+    sys.exit(0)
 
-	sys.exit(0)
+def on_publish(mosq, obj, msg):
+    pass
 
-if __name__== '__main__':
+def dataNetwork():
+    netdata = psutil.net_io_counters()
+    return netdata.packets_sent + netdata.packets_recv
 
-	signal.signal(signal.SIGINT, interruptHandler)
+def dataNetworkHandler():
+    idDevice = "ThisDevice"
+    mqttclient = paho.Client()
+    mqttclient.on_publish = on_publish
+    mqttclient.connect("test.mosquitto.org", 1883, 60)
+    while True:
+        packets = dataNetwork()
+        message = idDevice + " " + str(packets)
+        print "MQTT dataNetworkHandler " + message
+        mqttclient.publish("IoT101/Network", message)
+        time.sleep(1)
+
+if __name__ == '__main__':
+
+    signal.signal(signal.SIGINT, interruptHandler)
+
+    threadx = Thread(target=dataNetworkHandler)
+    threadx.start()
+
+    while True:
+        print "Hello Internet of Things 101"
+        time.sleep(5)
 
 
-	while True:
-
-		print "Hello Internet of Things 101"
-		time.sleep(5)
-
-
-#End of File
-
+# End of File
